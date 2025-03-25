b7d9e71e155d4a0e72361824510265602d457d30
CEGriffin <20ceg4@queensu.ca>
2025-03-25 11:02:47 -0400

    theres something wrong with this tree

--- Cluster_tree.R
+++ Cluster_tree.R
@@ -6,16 +6,12 @@ getwd()
 setwd("C:/Users/clari/Desktop/BIOL 432/Week10/432-Final-Project")
 
 #load libraries
-library(BiocManager)
-install("Biostrings")
-install("ggtree")
-install("annotate")
-install("ape")
-install("remotes")
-install("gmbecker/genbankr")
 library(ggplot2)
+library(ape)
 library(reshape2)
 library(viridis)
+library(dplyr)
+library(ggtree)
 
 #load data
 data<-read.csv("./DataSynth_expand_tag.csv")
@@ -28,23 +24,18 @@ head(data)
 #Relative growth rate 1-4
 #chlorophyl A and B concentration
 #gluc_cong, flav_conc
-#gm_number of leaves_initial
-#gm_leaf_len_initial
-#gm_totalleaf_area
+#leaf len and width and total area
+#all bolt stuff
+#all gm leaf dimensions
+#mortality
 
-#make sure they are all the right classes
-str(data)
-as.factor(data$population)
-as.factor(data$common_garden)
-#realtive growth rates, chlorA and B, and gluc and flav data are already numeric
-#number of leaves initial and leaf len initial and gm totalleaf area are already integers
 
 #make a dataframe with only the important columns
 data2<-data%>%
-  select("common_garden", "RGR1", "RGR2", "RGR3", "RGR4", "ChlorA", "ChlorB",
-         "gluc_Conc", "flav_Conc", "GM_NumberOfLeaves_Initial", "GM_Leaf_Len_Initial",
-         "GM_TotalLeaf_Area")
-head(data2)
+  select("RGR1", "RGR2", "RGR3", "RGR4", "ChlorA", "ChlorB",
+         "gluc_Conc", "flav_Conc", "GM_Leaf_Wid", "GM_Leaf_Len",
+         "GM_TotalLeaf_Area", "GM_NumberOfLeaves", "mortality")
+View(data2)
 
 #male a distance matrix - this is a linear matrix
 distance<-dist(data2)
@@ -58,8 +49,17 @@ head(pdat)
 names(pdat)<-c("query", "subject", "distance")
 dim(dist_matr)
 dim(pdat)
+str(pdat)
 
 #visualize distance matrix
 ggplot(data=pdat, aes(x=query, y=subject, fill=distance)) +
   geom_tile()
-2+2
+#i think this is too big to visualize- it keeps crashing- also uninterpretable
+
+#tree building
+tree1<-nj(distance)
+ggtree(tree1, layout="rectangular", ignore.negative.edge=TRUE)
+#has negative edge lengths??
+#something is wrong with this tree
+
+#try to colour by population or common garden?
