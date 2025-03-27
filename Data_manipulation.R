fb2cec1bb056163932d8cda19a28d3271247c516
CEGriffin <20ceg4@queensu.ca>
2025-03-27 09:23:15 -0400

    added common garden

--- Data_manipulation.R
+++ Data_manipulation.R
@@ -114,7 +114,7 @@ bolt_data<-bolt_data%>%
          GM_NumberOfLeaves=ifelse(is.na(GM_NumberOfLeaves),mean(data$GM_NumberOfLeaves, na.rm=T),GM_NumberOfLeaves))
 
 #Select out needed data
-data<-select(data, "Tag", "petri_dish", "population", "family", "ID", "gh_bench", "gh_col", "gh_row", "Row_Field", "Col_Field", starts_with("z_"), "mortality")
+data<-select(data, "Tag", "petri_dish", "population", "family", "common_garden", "ID", "gh_bench", "gh_col", "gh_row", "Row_Field", "Col_Field", starts_with("z_"), "mortality")
 
 #bolt data check for normality - needs log transformation
 long_bolt_data<-pivot_longer(bolt_data, 18:21, names_to="metric", values_to="values")
