041cd8d0fe60737751dbdae6148adef9f47691e3
EmiliagG <emiliagenvieve@gmail.com>
2025-03-29 22:46:25 -0400

    Updating the PCA to assess whether adding the bolt data (for only plants with mortality == 1) exhibits the same dimensionality as the full PCA (without bolt columns)

--- PCA.R
+++ PCA.R
@@ -6,13 +6,19 @@ plantData<-read.table("https://raw.githubusercontent.com/CEGriffin/432-Final-Pro
 
 head(plantData)
 
-plantData<-subset(plantData, mortality == 1)
+#plantData<-subset(plantData, mortality == 1)
 
-pcaData<-plantData %>%
+plantDataQ<-subset(plantData, common_garden == "Q")
+plantDataN<-subset(plantData, common_garden == "N")
+plantDataV<-subset(plantData, common_garden == "V")
+
+pcaData<-rbind(plantDataQ, plantDataN, plantDataV)
+
+pcaData<-pcaData %>%
   select(-c("Tag", "petri_dish", "population", "family", "ID",
-            "gh_bench", "gh_col", "gh_row", "Row_Field", "Col_Field", "mortality", "common_garden"))
+            "gh_bench", "gh_col", "gh_row", "Row_Field", "Col_Field", "common_garden"))
 
-plantPCA<-princomp(pcaData,cor=F)
+plantPCA<-princomp(na.omit(pcaData),cor=F)
 
 #checking the PCA output and its structure
 str(plantPCA)
@@ -21,7 +27,7 @@ names(plantPCA)
 
 library(ggplot2)
 
-PCloadings<-data.frame(Component=c(1:12),Eigenvalue=plantPCA$sdev^2)
+PCloadings<-data.frame(Component=c(1:13),Eigenvalue=plantPCA$sdev^2)
 
 #scree plot - elbow is around 5.0 principal components
 ggplot(aes(x=Component,y=Eigenvalue),data=PCloadings) +geom_point() + geom_line()
@@ -30,27 +36,83 @@ ggplot(aes(x=Component,y=Eigenvalue),data=PCloadings) +geom_point() + geom_line(
 plantPCA$loadings
 
 #combining the PCA with the original plant data for graphing 
-plantCombined<-cbind(plantData, plantPCA$scores)
+plantCombined<-cbind(na.omit(pcaData), plantPCA$scores)
 
 plantCombined<-plantCombined %>% 
   rename(PC1 = Comp.1, PC2 = Comp.2, PC3 = Comp.3, PC4= Comp.4, PC5 = Comp.5)
 
 str(plantCombined)
 
-## is there a reason the plantData does not have common garden?
-
 plantCombined$population<-as.factor(plantCombined$population)
 
 #creating the bivariate plots for PC1 vs PC2 
-ggplot(aes(x=PC1,y=PC2,colour=population),data=plantCombined)+
-  geom_point()
+ggplot(aes(x=PC1,y=PC2,colour=population, shape=common_garden),data=plantCombined)+
+  geom_point() + theme_bw()
 
-#ggplot(aes(x=PC1,y=PC2,colour=population, shape=common_garden),data=plantCombined)+
-  #geom_point()
+ggplot(aes(x=PC2,y=PC3,colour=population, shape=common_garden),data=plantCombined)+
+  geom_point() + theme_bw()
+
+ggplot(aes(x=PC3,y=PC4,colour=population, shape=common_garden),data=plantCombined)+
+  geom_point()+ theme_bw()
+
+ggplot(aes(x=PC4,y=PC5,colour=population, shape=common_garden),data=plantCombined)+
+  geom_point()+ theme_bw()
 
 
+#checking if the bolt only data exhibits the same trend 
 
+boltData<-read.table("https://raw.githubusercontent.com/CEGriffin/432-Final-Project/main/normalized_bolt_data.csv", 
+                      header=T, sep="," )
 
+head(boltData)
 
+boltData2<-cbind(boltData, common_garden= plantData$common_garden)
 
+pcaData2<-boltData2 %>%
+  select(-c("Tag", "petri_dish", "population", "family", "ID",
+            "gh_bench", "gh_col", "gh_row", "Row_Field", "Col_Field", 
+            "common_garden"))
+
+boltPCA<-princomp(pcaData2,cor=F)
+
+#checking the PCA output and its structure
+str(boltPCA)
+head(boltPCA)
+names(boltPCA)
+
+PCloadings2<-data.frame(Component=c(1:17),Eigenvalue=boltPCA$sdev^2)
+
+#scree plot - elbow is around 5.0 principal components
+ggplot(aes(x=Component,y=Eigenvalue),data=PCloadings2) +geom_point() + geom_line()
+
+#what are the loadings of ea. var on the components 
+boltPCA$loadings
+
+#combining the PCA with the original plant data for graphing 
+boltCombined<-cbind(boltData2, boltPCA$scores)
+
+boltCombined<-boltCombined %>% 
+  rename(PC1 = Comp.1, PC2 = Comp.2, PC3 = Comp.3, PC4= Comp.4, PC5 = Comp.5, PC6=Comp.6)
+
+str(boltCombined)
+
+## is there a reason the plantData does not have common garden?
+
+boltCombined$population<-as.factor(boltCombined$population)
+
+#creating the bivariate plots for PC1 vs PC2 
+ggplot(aes(x=PC1,y=PC2,colour=population, shape=common_garden),data=boltCombined)+
+  geom_point()+ theme_bw() 
+
+ggplot(aes(x=PC2,y=PC3,colour=population, shape=common_garden),data=boltCombined)+
+  geom_point()+ theme_bw()
+
+ggplot(aes(x=PC3,y=PC4,colour=population, shape=common_garden),data=boltCombined)+
+  geom_point()+ theme_bw()
+
+ggplot(aes(x=PC4,y=PC5,colour=population, shape=common_garden),data=boltCombined)+
+  geom_point()+ theme_bw()
+
+ggplot(aes(x=PC5,y=PC6,colour=population, shape=common_garden),data=boltCombined)+
+  geom_point()+ theme_bw()
 
